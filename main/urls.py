363fea9c91b80e1d5b92faa77252f478afd2b760
antoliny0919 <antoliny0919@gmail.com>
2025-09-03 18:16:12 +0900

    Update the path of the admin to be modified to after_admin.

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("after_admin/", admin.site.urls),
     path("compare/", include("compare.urls")),
 ]
