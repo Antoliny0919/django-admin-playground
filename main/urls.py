55603240fc82e3730efffb68a9890d70420c2735
antoliny0919 <antoliny0919@gmail.com>
2025-09-03 19:08:57 +0900

    Add before admin for compare.

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .admin import before_site
+
 urlpatterns = [
+    path("before_admin/", before_site.urls),
     path("after_admin/", admin.site.urls),
     path("compare/", include("compare.urls")),
 ]
