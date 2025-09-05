608b52be11671698f2203f7a1f0722f3be462b16
antoliny0919 <antoliny0919@gmail.com>
2025-09-05 10:46:31 +0900

    Update register wrapper.

--- main/utils.py
+++ main/utils.py
@@ -4,11 +4,6 @@ from .admin import before_site
 MY_ADMINS = [admin.site, before_site]
 
 
-def register(model, model_admin=None, inlines=[]):
+def register(model, model_admin=None, **kwargs):
     for site in MY_ADMINS:
-        if inlines:
-            site.register(model, inlines=inlines)
-        elif model_admin:
-            site.register(model, model_admin)
-        else:
-            site.register(model)
+        site.register(model, model_admin, **kwargs)
