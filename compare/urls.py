f4917f850a81a347819584d10eed0ecab1bf7058
antoliny0919 <antoliny0919@gmail.com>
2025-09-07 16:15:01 +0900

    Add BaseView to inject app_list into context.

--- compare/urls.py
+++ compare/urls.py
@@ -1,17 +1,29 @@
 from django.urls import path
-from django.views.generic import TemplateView
+
+from .views import (
+    IndexView,
+    LoginView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    AppIndexView,
+    ChangeListView,
+    AddView,
+    ChangeView,
+    HistoryView,
+    DeleteView,
+)
 
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="compare/index.html")),
-    path("login/", TemplateView.as_view(template_name="compare/login.html")),
-    path("password_change/", TemplateView.as_view(template_name="compare/password_change.html"), name="password_change"),
-    path("password_change/done/", TemplateView.as_view(template_name="compare/password_change_done.html")),
-    path("<str:app_name>/", TemplateView.as_view(template_name="compare/app_index.html")),
+    path("", IndexView.as_view()),
+    path("login/", LoginView.as_view()),
+    path("password_change/", PasswordChangeView.as_view(), name="password_change"),
+    path("password_change/done/", PasswordChangeDoneView.as_view()),
+    path("<str:app_name>/", AppIndexView.as_view()),
 
-    path("<str:app_name>/<str:model_name>/", TemplateView.as_view(template_name="compare/changelist.html")),
-    path("<str:app_name>/<str:model_name>/add/", TemplateView.as_view(template_name="compare/add.html")),
-    path("<str:app_name>/<str:model_name>/<int:pk>/change/", TemplateView.as_view(template_name="compare/change.html")),
-    path("<str:app_name>/<str:model_name>/<int:pk>/history/", TemplateView.as_view(template_name="compare/history.html")),
-    path("<str:app_name>/<str:model_name>/<int:pk>/delete/", TemplateView.as_view(template_name="compare/delete.html")),
+    path("<str:app_name>/<str:model_name>/", ChangeListView.as_view()),
+    path("<str:app_name>/<str:model_name>/add/", AddView.as_view()),
+    path("<str:app_name>/<str:model_name>/<int:pk>/change/", ChangeView.as_view()),
+    path("<str:app_name>/<str:model_name>/<int:pk>/history/", HistoryView.as_view()),
+    path("<str:app_name>/<str:model_name>/<int:pk>/delete/", DeleteView.as_view()),
 ]
