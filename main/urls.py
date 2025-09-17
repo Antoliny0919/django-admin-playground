80985803be60a66233d24b9ce935305fd0d32fc9
antoliny0919 <antoliny0919@gmail.com>
2025-09-17 15:59:30 +0900

    Apply before/after to templates rendered with InclusionAdminNode.

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
+from .admin import after_site
 from .admin import before_site
 
 urlpatterns = [
     path("before_admin/", before_site.urls),
-    path("after_admin/", admin.site.urls),
+    path("after_admin/", after_site.urls),
     path("compare/", include("compare.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
