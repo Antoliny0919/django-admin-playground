431fab0dbcb47761319cb867053282125515a1d1
antoliny0919 <antoliny0919@gmail.com>
2025-11-10 20:08:09 +0900

    feat: Add screenshot capture for specific image
    - Add 'name' argument to specify image to capture
    - Prepare method to generate shot-scraper command

--- docs/management/commands/makeimages.py
+++ docs/management/commands/makeimages.py
@@ -1,16 +1,28 @@
 import subprocess
 import time
-from django.core.management.base import BaseCommand
+
+from django.core.management.base import BaseCommand, CommandError
 
 PORT_NUMBER = "8009"
+
+IMAGE_DATA = {
+    "admin01t": {
+        "path": "after_admin/login/",
+        "selector": "#container",
+        "output": "admin01t.png",
+        "width": "394",
+    }
+}
 
 
 class Command(BaseCommand):
     help = "Generates images used in the Django documentation."
     url = f"http://localhost:{PORT_NUMBER}"
+    screen_width = 1200
+    screen_height = 768
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument("name", help="Name of the image to generate")
 
     def start_server(self):
         self.server = subprocess.Popen(
@@ -18,9 +30,29 @@ class Command(BaseCommand):
         )
         time.sleep(3)  # wait for server to be ready
 
+    def create_shot_scraper_command(self, path, selector, output, width, **kwargs):
+        return [
+            "shot-scraper",
+            f"{self.url}/{path}",
+            "-s",
+            selector,
+            "-o",
+            output,
+            "-w",
+            width,
+        ]
+
     def handle(self, *args, **options):
+        name = options["name"]
+        if name not in IMAGE_DATA.keys():
+            raise CommandError(f"{name} is an image that cannot be generated")
         self.start_server()
-        subprocess.run([
-            "shot-scraper", f"{self.url}/after_admin/"
-        ])
+        data = IMAGE_DATA[name]
+        command = self.create_shot_scraper_command(
+            data["path"],
+            data["selector"],
+            data["output"],
+            data["width"]
+        )
+        subprocess.run(command)
         self.server.terminate()
--- docs/tests/test_management.py
+++ docs/tests/test_management.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.core.management import call_command
 from unittest.mock import patch, MagicMock
 
+from docs.management.commands.makeimages import Command
+
 
 class MakeImageManagementCommandTestCase(TestCase):
 
@@ -12,10 +14,32 @@ class MakeImageManagementCommandTestCase(TestCase):
         mock_server = MagicMock()
         mock_popen.return_value = mock_server
 
-        call_command("makeimages")
+        call_command("makeimages", "admin01t")
 
         mock_popen.assert_called_once_with(
             ["python", "manage.py", "runserver", "8009"]
         )
         mock_sleep.assert_called_once_with(3)
         mock_server.terminate.assert_called_once()
+
+    def test_create_shot_scraper_command(self):
+        command = Command()
+        shot_scraper_command = command.create_shot_scraper_command(
+            "one/two/",
+            ".one div.two",
+            "numbers.png",
+            "400"
+        )
+        self.assertEqual(
+            shot_scraper_command,
+            [
+                "shot-scraper",
+                "http://localhost:8009/one/two/",
+                "-s",
+                ".one div.two",
+                "-o",
+                "numbers.png",
+                "-w",
+                "400",
+            ]
+        )
