b865068aed08d45599799bdbdd0589ed7d093376
antoliny0919 <antoliny0919@gmail.com>
2025-11-15 22:54:43 +0900

    fix: Configure project to ignore W003 system check

--- main/settings.py
+++ main/settings.py
@@ -137,3 +137,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Silence template tag naming conflict warning
+SILENCED_SYSTEM_CHECKS = ["templates.W003"]
