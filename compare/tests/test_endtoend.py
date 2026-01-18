e96613cce8e1f4f625189d76d5817d325e32f492
antoliny0919 <antoliny0919@gmail.com>
2026-01-18 11:56:53 +0900

    fix: Auto theme not being applied

---
+++ compare/tests/test_endtoend.py
@@ -0,0 +1,33 @@
+import pytest
+from playwright.sync_api import expect
+
+
+@pytest.mark.playwright
+class TestCompareSidebarButtons:
+
+    def get_frame_htmls(self, auth_page):
+        before_frame = auth_page.locator("iframe#before").content_frame
+        after_frame = auth_page.locator("iframe#after").content_frame
+
+        return [before_frame.locator("html"), after_frame.locator("html")]
+
+    def test_theme_change(self, auth_page, live_server):
+        auth_page.goto(f"{live_server.url}/compare")
+
+        before_html, after_html = self.get_frame_htmls(auth_page)
+
+        expect(before_html).to_have_attribute("data-theme", "auto")
+        expect(after_html).to_have_attribute("data-theme", "auto")
+        theme_change_button = auth_page.get_by_test_id("theme-toggle-button")
+
+        theme_change_button.click()
+        expect(before_html).to_have_attribute("data-theme", "light")
+        expect(after_html).to_have_attribute("data-theme", "light")
+
+        theme_change_button.click()
+        expect(before_html).to_have_attribute("data-theme", "dark")
+        expect(after_html).to_have_attribute("data-theme", "dark")
+
+        theme_change_button.click()
+        expect(before_html).to_have_attribute("data-theme", "auto")
+        expect(after_html).to_have_attribute("data-theme", "auto")
