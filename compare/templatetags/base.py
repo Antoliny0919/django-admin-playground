a135ce297f414f82d55d919708d938168c23d1ba
antoliny-never-wrong <antoliny0919@gmail.com>
2026-03-01 22:29:51 +0900

    feat: Add custom include tag for path based template routing
    overriding include tag to dynamically change the template to render based on the url path

--- compare/templatetags/base.py
+++ compare/templatetags/base.py
@@ -1,5 +1,11 @@
+import contextlib
+
 from django.contrib.admin.templatetags.base import InclusionAdminNode
+from django.template import Library, TemplateDoesNotExist
 from django.template.library import InclusionNode
+from django.template.loader_tags import IncludeNode, construct_relative_path, do_include
+
+register = Library()
 
 
 class InclusionCompareAdminNode(InclusionAdminNode):
@@ -18,3 +24,34 @@ class InclusionCompareAdminNode(InclusionAdminNode):
             ],
         )
         return InclusionNode.render(self, context)
+
+
+class CustomIncludeNode(IncludeNode):
+    def render(self, context):
+        path = context.request.path
+        if "before_admin" in path:
+            original_name = self.template.resolve(context)
+            new_name = original_name.replace("admin", "before_admin")
+            original_key = (
+                construct_relative_path(self.origin.template_name, original_name),
+            )
+            new_key = (construct_relative_path(self.origin.template_name, new_name),)
+            cache = context.render_context.dicts[0].setdefault(self, {})
+            # Pre-populate the cache with the modified template
+            # before looking up the cache in IncludeNode render.
+            if original_key not in cache:
+                with contextlib.suppress(TemplateDoesNotExist):
+                    cache[original_key] = context.template.engine.select_template(
+                        new_key,
+                    )
+        return super().render(context)
+
+
+@register.tag("include")
+def custon_do_include(parser, token):
+    node = do_include(parser, token)
+    return CustomIncludeNode(
+        node.template,
+        extra_context=node.extra_context,
+        isolated_context=node.isolated_context,
+    )
---
+++ compare/tests/test_override_tag.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+
+from django.template import RequestContext, Template
+from django.test import RequestFactory, TestCase, override_settings
+
+BASE_DIR = Path(__file__).resolve().parent
+
+
+@override_settings(
+    TEMPLATES=[
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [
+                BASE_DIR / "templates",
+            ],
+            "APP_DIRS": True,
+            "OPTIONS": {
+                "context_processors": [
+                    "django.template.context_processors.request",
+                    "django.contrib.auth.context_processors.auth",
+                    "django.contrib.messages.context_processors.messages",
+                ],
+                "builtins": [
+                    "compare.templatetags.base",
+                ],
+            },
+        },
+    ],
+)
+class CustomIncludeTagTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.factory = RequestFactory()
+
+    def render_template(self, string, request, context=None):
+        context = context or {}
+        context = RequestContext(request, context)
+        return Template(string).render(context)
+
+    def test_after_admin_include_tag_render(self):
+        request = self.factory.get("/after_admin/some_path/")
+        rendered = self.render_template(
+            "{% include 'admin/test_template.html' %}",
+            request,
+        )
+        self.assertEqual(rendered, "<h1>Hello World After Admin!</h1>\n")
+
+    def test_before_admin_include_tag_render(self):
+        request = self.factory.get("/before_admin/some_path/")
+        rendered = self.render_template(
+            "{% include 'admin/test_template.html' %}",
+            request,
+        )
+        self.assertEqual(rendered, "<h1>Hello World Before Admin!</h1>\n")
+
+    def test_before_admin_include_tag_unknown_template_render(self):
+        request = self.factory.get("/before_admin/some_path/")
+        rendered = self.render_template(
+            "{% include 'admin/only_after.html' %}",
+            request,
+        )
+        self.assertEqual(
+            rendered,
+            "<span>This template only exists in after_admin</span>\n",
+        )
+
+    def test_before_admin_inner_include_tag_render(self):
+        request = self.factory.get("/before_admin/some_path/")
+        rendered = self.render_template(
+            "{% include 'admin/have_include.html' %}",
+            request,
+        )
+        self.assertEqual(
+            rendered,
+            "<h1>Hello World Before Admin!</h1>\n\n<div>have include!</div>\n",
+        )
